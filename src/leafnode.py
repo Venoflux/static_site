e3abe965bb9010c65a231d1aa41a97c8c7a9c86a
Venoflux <erolsarp@gmail.com>
2025-10-20 17:14:04 +0300

    parent node and related tests are created

--- src/leafnode.py
+++ src/leafnode.py
@@ -8,7 +8,7 @@ class LeafNode(HTMLNode):
     def to_html(self):
         if self.value is None:
             raise ValueError("All leaf nodes must have a value")
-        if self.tag is None:
+        elif self.tag is None:
             return f"{self.value}"
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
         
